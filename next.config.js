a8f354383cf6d12604246dcf8315af1a3071e295
Jose Sleiter Ríos <jose.sleiter@go1.com>
2022-09-01 04:10:19 -0400

    Feature: verify hook husky

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["loremflickr.com"],
-    deviceSizes: [320, 640, 660, 768, 1024, 1600],
-  },
+    deviceSizes: [320, 640, 660, 768, 1024, 1600]
+  }
 };
 
 module.exports = nextConfig;
